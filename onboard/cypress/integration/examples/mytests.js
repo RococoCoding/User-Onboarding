fa9c13afb7baa0a19b1d308c26f6871e831f3c53
Alice Chang <sinsofangels@gmail.com>
2020-10-14 20:02:30 -0400

    Add testing for button clickability if input is empty

--- onboard/cypress/integration/examples/mytests.js
+++ onboard/cypress/integration/examples/mytests.js
@@ -1,11 +1,11 @@
-describe("Test test suite", ()=> {
+describe("Test inputs", ()=> {
   before(()=>{
     cy.visit("")
   }) 
-  afterEach(()=> {
-    cy.get('button').click()
+  beforeEach(()=>{
+    cy.get('button').should("be.disabled")
   })
-  it("Checks name", ()=>{
+  it("Checks name and button", ()=>{
     cy.get('[type="text"]').type("Alice Chang").should("have.value", "Alice Chang")
   })
   it("Checks email", ()=>{
@@ -17,9 +17,11 @@ describe("Test test suite", ()=> {
   it("checks checkbox", ()=> {
     cy.get('[type="checkbox"]').check()
   })
-  it("checks form submit", ()=> {
-    cy.get('form').submit()
-  })  
 })
 
-// lakjdf
+describe("Checks filled in form", ()=> {
+  it("checks form submit and button clickability", ()=> {
+    cy.get('form').submit()
+    cy.get('button').click() 
+  })  
+})
